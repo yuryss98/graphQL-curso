4bb58918e20c4ee778d9ad4c1a5ba3d4f036236f
Yury Stolarz Santana <ysantana@telluscompany.com.br>
2024-08-16 15:14:24 -0300

    fix: file name

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -3,7 +3,7 @@ const { loadFilesSync } = require('@graphql-tools/load-files')
 const { join } = require('path')
 
 const allTypes = loadFilesSync(join(__dirname, 'modules', '**', '*.gql'));
-const allResolvers = loadFilesSync(join(__dirname, 'modules', '**', '*.resolvers.js'));
+const allResolvers = loadFilesSync(join(__dirname, 'modules', '**', 'resolvers.js'));
 
 module.exports = {
   typeDefs: mergeTypeDefs(allTypes),
