b078890ec8ac84de8cd012d432f6317bf670c482
Yury Stolarz Santana <ysantana@telluscompany.com.br>
2024-08-17 14:42:42 -0300

    fix: add format error

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const { ApolloServer } = require("apollo-server")
 const { typeDefs, resolvers } = require('./src/graphql')
 
-const server = new ApolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  formatError: (err) => {
+    return new Error(err.message)
+  }
+})
 
 server.listen(8000).then(({ url }) => console.log(`🚀 Server ready at ${url}`))
